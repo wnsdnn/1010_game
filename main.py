631d55fa6e477c8fbb9fee8a66b536a8c6585036
wnsdnn <a24369029@gmail.com>
2021-10-28 19:18:13 +0900

    10/28

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from os import O_NDELAY
 import pygame
 import random
+import math
 
 pygame.init()
 
@@ -9,11 +9,6 @@ screen_width = 550
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# xx = 0
-# yy = 0
-# start_x = 0
-# start_y = 0
-# md = False
 
 game_font = pygame.font.Font(None, 40)
 
@@ -21,42 +16,65 @@ pygame.display.set_caption("1010 game")
 
 clock = pygame.time.Clock()
 
-
+# 게임이 실행중인지를 판별하는 변수
 running = True
-FILED = []
-FILED_WIDTH = 8
-FILED_HEIGHT = 8
-FILED_COLOR = (0, 0, 0)
-FILED_PIECE_SIZE = 45
+
+# 게임판 관련 변수들
+FILED = []  # 게임판
+FILED_WIDTH = 8  # 게임판의 넓이
+FILED_HEIGHT = 8  # 게임판의 높이
+FILED_COLOR = (0, 0, 0)  # 게임판의 색깔
+FILED_PIECE_SIZE = 45  # 게임판에 들어가는 사각형의 크기
+
+# x, y 좌표
 FILED_X_POS = (screen_width / 2) - (FILED_PIECE_SIZE * FILED_WIDTH) / 2
 FILED_Y_POS = FILED_PIECE_SIZE * 2
+# 도형들의 y좌표
+block_y_pos = screen_height - 190
+# 점수
 score = 0
 score_str = "Score: %s" % (score)
 
+# 도형의 기본 크기 3*3
+block_width = 3
+block_height = 3
 
-# class Block():
-#     def __init__(self, num, xpos, ypos, id):
-#         self.id = id
-#         self.image = pygame.image.load("C:\\Users\\User\\Desktop\\1010_game\\image\\block%s.png" % (num+1))
-#         self.clicked = False
-#         self.xpos = xpos
-#         self.ypos = ypos
-#         self.rect = self.image.get_rect()
+# 도형의 모양(번호)?이 랜덤으로 들어갈 리스트
+random_block = []
 
+# 도형이 들어갈 리스트
+block_list = []
+
+# 마우스를 클릭한 좌표
+start_to_x = 0
+start_to_y = 0
+
+# 현재 마우스의 좌표
+mouse_to_x = 0
+mouse_to_y = 0
+
+# 마우스 다운 되었있는지를 판별하는 함수
+md = False
+
+# 도형들의 인덱스 번호와 모양 정보가 있는 변수들
+block_id = 100
+block_num = 100
+
+# 도형들의 모양들
 blocks = (
     ((0, 0, 1),     
-    (0, 0, 1), 
-    (1, 1, 1)),
+     (0, 0, 1), 
+     (1, 1, 1)),
     ((0, 0, 0),
      (0, 1, 0),
      (0, 1, 0)),
-     ((0, 0, 0),
+    ((0, 0, 0),
      (0, 0, 0),
      (1, 1, 1)),
     ((1, 1, 1),     
-    (1, 1, 1), 
-    (1, 1, 1)),
-     ((1, 1, 1),
+     (1, 1, 1), 
+     (1, 1, 1)),
+    ((1, 1, 1),
      (0, 0, 1),
      (0, 0, 1)),
     ((0, 0, 0),
@@ -70,9 +88,27 @@ blocks = (
      (1, 1, 1)),
 )
 
-shape_width = 3
-shape_height = 3
-random_block = []
+# 도형들 만드는 클래스
+class Block():
+    def __init__(self, xpos, ypos, id, num, list):
+        self.rect = 0
+        self.id = id
+        self.xpos = xpos
+        self.ypos = ypos
+        self.num = num
+        self.list = [list[0], list[1]]
+
+    def get_rect(self, rect):
+        self.rect = rect
+
+    def draw_block(self, mouse_xpos, mouse_ypos, id):
+        xpos = 0
+        ypos = 0
+        if self.id == id:
+            xpos = mouse_xpos
+            ypos = mouse_ypos
+        block = pygame.draw.rect(screen, (0, 0, 0), (self.xpos + xpos, self.ypos + ypos, FILED_PIECE_SIZE, FILED_PIECE_SIZE))
+        return block
 
 
 # 게임판 정보를 FILED라는 리스트 안에 집어넣기
@@ -91,14 +127,11 @@ def draw_filed():
             if val != 0:
                 line_width = 0
 
-            pygame.draw.rect(screen, (0, 0, 0),
-            (
-                FILED_X_POS + x*FILED_PIECE_SIZE,
-                FILED_Y_POS + y*FILED_PIECE_SIZE,
+            pygame.draw.rect(screen, (0, 0, 0), (
+                FILED_X_POS + y*FILED_PIECE_SIZE,
+                FILED_Y_POS + x*FILED_PIECE_SIZE,
                 FILED_PIECE_SIZE,
-                FILED_PIECE_SIZE
-            )
-            , line_width)
+                FILED_PIECE_SIZE ), line_width)
 
 # 게임에 사용할 도형 3개 배열에 넣는 함수
 def set_block():
@@ -106,24 +139,26 @@ def set_block():
         random_block.append(random.randrange(0, 8))
 
 
-# 처음 세팅할 도형을 그려주는 함수
+# 도형을 그려 넣는 함수
 def draw_block():
-    block_height = screen_height - 190
     for idx, val in enumerate(tuple(random_block)):
-        for y in range(shape_height):
-            for x in range(shape_width):
+        for y in range(block_height):
+            for x in range(block_width):
                 if blocks[val][y][x] == 0:
                     continue
-                pygame.draw.rect(screen, (0, 0, 0), (
-                        FILED_PIECE_SIZE*x + (idx*FILED_PIECE_SIZE*3.7) + 50
-                        , FILED_PIECE_SIZE*y + block_height, FILED_PIECE_SIZE, FILED_PIECE_SIZE
-                ), 1)
+                block = Block(FILED_PIECE_SIZE*x + (idx*FILED_PIECE_SIZE*3.7) + 50, FILED_PIECE_SIZE*y + block_y_pos, idx, val, [y, x])
+                block_rect = block.draw_block(mouse_to_x, mouse_to_y, block_id)
+                block.get_rect(block_rect)
+                block_list.append(block)
 
+
+def fff(num):
+    result = num / FILED_PIECE_SIZE
+    return math.floor(result)
 
 # 게임 실행
 set_filed()
 
-print(random_block)
 while running:
     dt = clock.tick(60)
 
@@ -131,43 +166,60 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # 마우스 이벤트
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        md = True
+        start_to_x = pygame.mouse.get_pos()[0]
+        start_to_y = pygame.mouse.get_pos()[1]
+
+        for block in block_list:
+            if block.rect.collidepoint(event.pos):
+                block_id = block.id
+                block_num = block.num
+
+        # for block in blocks[block_num]:
+        #     print(block)
+        
+
+    if md and event.type ==  pygame.MOUSEMOTION:
+        mouse_to_x = pygame.mouse.get_pos()[0] - start_to_x
+        mouse_to_y = pygame.mouse.get_pos()[1] - start_to_y
 
 
+    if event.type == pygame.MOUSEBUTTONUP:
+        # 게임판 기준으로 현재 마우스의 위치
+        block_x = pygame.mouse.get_pos()[0] - FILED_X_POS
+        block_y = pygame.mouse.get_pos()[1] - FILED_Y_POS
+
+        result = (fff(block_x), fff(block_y))
+        print(result)
+        
+
+        md = False
+        start_to_x = 0
+        start_to_y = 0
+        mouse_to_x = 0
+        mouse_to_y = 0
+        block_id = 100
+        block_num = 100
+    
     # 배경색 칠하기
     screen.fill((225, 255, 255))
 
-    
-
-    # 마우스 이벤트
-    # if event.type == pygame.MOUSEBUTTONDOWN:
-    #     if rect_box.collidepoint(event.pos):
-    #         start_x = pygame.mouse.get_pos()[0]
-    #         start_y = pygame.mouse.get_pos()[1]
-    #         md = True
-
-    # if md and event.type ==  pygame.MOUSEMOTION:
-    #     xx = pygame.mouse.get_pos()[0] - start_x
-    #     yy = pygame.mouse.get_pos()[1] - start_y
-
-    # if event.type == pygame.MOUSEBUTTONUP:
-    #     md = False
-    #     xx = 0
-    #     yy = 0
-    #     start_x = 0
-    #     start_y = 0
-
-    
     # 현재 점수 나타내기
     total_score = game_font.render(score_str, True, (0, 0, 0))
     screen.blit(total_score, (FILED_PIECE_SIZE/2, FILED_PIECE_SIZE/2))
 
-    # 게임판 그리는 함수
-    if len(random_block) <= 0:
+    # 도형 3개를 다 쓰면 다시 도형 만드는 함수
+    if len(random_block) <= 0:  
         set_block()
 
+    # 게임판이랑 도형 그리기
     draw_filed()
     draw_block()
     
+
+    # running = False
     pygame.display.update()
 
     
